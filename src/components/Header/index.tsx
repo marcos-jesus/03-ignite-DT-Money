e567f866422d7b0f2a7ea4694a01f60d953122bb
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-05 06:34:27 -0300

    Implement Radix components, create styleds components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { NewTransactionModal } from '../NewTransactionModal'
+
 
 import logoImg from '../../assets/logo.svg'
 
@@ -8,7 +12,14 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton> Nova transação </NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton> Nova transação </NewTransactionButton>
+          </Dialog.Trigger>
+
+            <NewTransactionModal />
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
